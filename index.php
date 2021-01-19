896921e04651688fe42939f7c7520626c1efb695
MarcoB <mrc.bsllt@gmail.com>
2021-01-19 15:26:51 +0100

    Aggiunto stile e lunghezza dei paragrafi

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
   $text =
 "
-Osteria N.1 in casin non c’è nessuno... ci son solo preti e frati che si inculano beati dammela a me biondina dammela a me biondaa...<br>
-Osteria N.2 le mie gambe fra le tue,le tue gambe fra le mie fanno mille porcherie dammela a me biondina dammela a me biondaa...<br>
+Osteria N.1 in casin non c’è nessuno... ci son solo preti e frati che si <span>inculano</span> beati dammela a me biondina dammela a me biondaa...<br>
+Osteria N.2 le mie gambe fra le tue,le tue gambe fra le mie fanno mille <span>porcherie</span> dammela a me biondina dammela a me biondaa...<br>
 Osteria N.3 la peppina fa il caffè, fa il caffè alla tirolese con le pezze del marchese dammela a me biondina dammela a me biondaa...<br>
-Osteria N.4, la marchesa aveva un gatto, con la coda del felino si faceva un ditalino dammela a me biondina dammela a me biondaa...<br>
-Osteria N.5 c’è chi perde e c’è che vince, ma chi perde caso strano se lo trova dentro l’ano dammela a me biondina dammela a me biondaa...<br>
+Osteria N.4, la marchesa aveva un gatto, con la coda del felino si faceva un <span>ditalino</span> dammela a me biondina dammela a me biondaa...<br>
+Osteria N.5 c’è chi perde e c’è che vince, ma chi perde caso strano se lo trova dentro l’<span>ano</span> dammela a me biondina dammela a me biondaa...<br>
 Osteria N.7, il salame piace a fette, ma alle donne caso strano il salame piace sano dammela a me biondina dammela a me biondaa...<br>
-Osteria N.20 se la figa avesse i denti, quanti cazzi all’ospedale quante fighe in tribunale dammela a me biondina dammela a me biondaaa...<br>
-Osteria N.30, chi dà il culo non si penta, oggigiorno caso strano va di moda il deretano dammela a me biondina dammela a me biondaa...<br>
-Osteria N.100,se la figa andasse a vento... quanti cazzi in alto mare tu vedresti navigare dammela a me biondina dammela a me biondaa...<br>
-Osteria degli untori, hanno tutti il cazzo fuori, e li danno alle colleghe per non farsi delle seghe dammela a me biondina dammela a me biondaaaaa.
+Osteria N.20 se la <span>figa</span> avesse i denti, quanti <span>cazzi</span> all’ospedale quante fighe in tribunale dammela a me biondina dammela a me biondaaa...<br>
+Osteria N.30, chi dà il <span>culo</span> non si penta, oggigiorno caso strano va di moda il <span>deretano</span> dammela a me biondina dammela a me biondaa...<br>
+Osteria N.100,se la <span>figa</span> andasse a vento... quanti <span>cazzi</span> in alto mare tu vedresti navigare dammela a me biondina dammela a me biondaa...<br>
+Osteria degli untori, hanno tutti il <span>cazzo</span> fuori, e li danno alle colleghe per non farsi delle <span>seghe</span> dammela a me biondina dammela a me biondaaaaa.
 ";
-  $words_to_replace = ["inculano", "porcherie", "ditalino", "ano", "figa", "cazzo", "cazzi", "culo", "deretano", "seghe"];
+  $words_to_replace = [
+    "inculano", "porcherie", "ditalino", "deretano", "figa", "cazzo", "cazzi", "culo", "ano", "seghe"
+  ];
+  $censured_text = str_replace($words_to_replace, $_GET["word"], $text);
  ?>
 
  <!DOCTYPE html>
@@ -21,14 +24,32 @@ Osteria degli untori, hanno tutti il cazzo fuori, e li danno alle colleghe per n
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Yusei+Magic&display=swap" rel="stylesheet">
+   <!-- foglio di stile personale -->
+   <link rel="stylesheet" href="css/style.css">
    <title>php bad words</title>
  </head>
 
  <body>
+
    <h2>Testo originale</h2>
-   <p><?php echo $text; ?></p>
+   <div class="container">
+     <p><?php echo $text; ?></p>
+     <div class="length">
+      <span><?php echo strlen($text); ?></span>
+       <span>Caratteri</span>
+     </div>
+   </div>
 
    <h2>Testo censurato</h2>
-   <p><?php echo str_replace($words_to_replace, $_GET["word"], $text); ?></p>
+   <div class="container">
+     <p><?php echo $censured_text; ?></p>
+     <div class="length">
+       <span><?php echo strlen($censured_text); ?></span>
+       <span>Caratteri</span>
+     </div>
+   </div>
  </body>
  </html>
